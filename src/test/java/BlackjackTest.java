06c148252fdd42c12760d56a0b0e5875a6de7e6b
Vishahan Thilagakumar <vishahan@live.com>
2018-09-15 17:39:34 -0400

    Set up user selection test case.
    Attempted to simulate user input, have yet to verify if it works.

--- src/test/java/BlackjackTest.java
+++ src/test/java/BlackjackTest.java
@@ -1,3 +1,6 @@
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import junit.framework.*;
 
 public class BlackjackTest extends TestCase{
@@ -5,11 +8,43 @@ public class BlackjackTest extends TestCase{
 	//Tests temporarily hardcoded while I adjust to TDD
 	public void testGameStartMode() {
 		Blackjack Game = new Blackjack();
-		//Game mode selection should return whatever enum Blackjack.GameUsing.File, Blackjack.GameUsing.Console or Blackjack.GameUsing.Error
-		assertEquals(Blackjack.GameUsing.File, Game.SetGameContols('f'));
-		assertEquals(Blackjack.GameUsing.Console, Game.SetGameContols('c'));
-		assertEquals(Blackjack.GameUsing.Error, Game.SetGameContols('d'));
+		//Game mode selection should change Game.GetGameInputMode() to whatever enum Blackjack.GameUsing.File, Blackjack.GameUsing.Console or Blackjack.GameUsing.Error
+		
+		//File
+		Game.SetGameContols('f');
+		assertEquals(Blackjack.GameUsing.File, Game.GetGameInputMode());
+		
+		//Console
+		Game.SetGameContols('c');
+		
+		
+		//Invalid
+		Game.SetGameContols('d');
+		assertEquals(Blackjack.GameUsing.Error, Game.GetGameInputMode());
 	}
 	
-	
+	public void testUserGameSelection() {
+		//Overriding system.in to simulate user input.
+		InputStream UserEnterF_Stream = new ByteArrayInputStream("f\n".getBytes());
+		InputStream UserEnterC_Stream = new ByteArrayInputStream("c\n".getBytes());
+		InputStream UserEnterD_Stream = new ByteArrayInputStream("d\n".getBytes());
+		
+		Blackjack UserEnterF_Game = new Blackjack();
+		Blackjack UserEnterC_Game = new Blackjack();
+		Blackjack UserEnterD_Game = new Blackjack();
+		
+		System.setIn(UserEnterF_Stream);
+		UserEnterF_Game.start();
+		assertEquals(Blackjack.GameUsing.File, UserEnterF_Game.GetGameInputMode());
+		
+		System.setIn(UserEnterC_Stream);
+		UserEnterC_Game.start();
+		assertEquals(Blackjack.GameUsing.Console, UserEnterC_Stream.GetGameInputMode());
+		
+		System.setIn(UserEnterD_Stream);
+		UserEnterD_Game.start();
+		assertEquals(Blackjack.GameUsing.Error, UserEnterD_Stream.GetGameInputMode());
+		
+		System.setIn(System.in);
+	}
 }
